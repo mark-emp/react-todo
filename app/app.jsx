9020f81b30da2f1f76f725228bb05770b0c0ac2a
Mark Patton <mark@emp-technical.com>
2016-09-06 07:35:35 +0100

    Update to include testing and foundation varibale styling

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
 
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
@@ -11,10 +10,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <div>
-    <Router history={hashHistory}>
-      <Route path="/" component={Main}></Route>
-    </Router>
-  </div>,
+  <p>Boilerplate React Project</p>,
   document.getElementById('app')
 );
