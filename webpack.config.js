93d39478cf24d56a18829636d435a6c583e4fe0d
Mark Patton <mark@emp-technical.com>
2016-09-06 19:40:38 +0100

    Setup and create root component

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,13 @@ module.exports = {
   },
   resolve: {
     root: __dirname,
+    modulesDirectories: [
+      'node_modules',
+      './app/components'
+    ],
     alias: {
       applicationStyles: 'app/styles/app.scss',
+
     },
     extensions: ['', '.js', '.jsx']
   },
