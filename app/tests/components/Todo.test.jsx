607117a12716062701126537d79c21cd0271ca71
Mark Patton <mark@emp-technical.com>
2016-09-11 10:23:30 +0100

    Add toggle support

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -11,4 +11,21 @@ describe('Todo', () => {
     expect(Todo).toExist();
   });
 
+  it('should call onToggle prop with id on click', () => {
+    var todoData = {
+                    id: 100,
+                    text: 'Test text',
+                    completed: true
+                  };
+
+    var spy = expect.createSpy();
+    var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}/>);
+
+    var $el = $(ReactDOM.findDOMNode(todo));
+    TestUtils.Simulate.click($el[0]);
+
+    expect(spy).toHaveBeenCalledWith(todoData.id);
+
+  });
+
 });
